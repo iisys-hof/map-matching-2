4ff70e3187b590ba9fe7d972242f11462ac0f1fc
Adrian Woeltche <adrian.woeltche@iisys.de>
2025-05-13 17:41:57 +0200

    updated copyright

--- src/library/src/types/geometry/rich_type/rich_line.cpp
+++ src/library/src/types/geometry/rich_type/rich_line.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 Adrian Wöltche
+// Copyright (C) 2024-2025 Adrian Wöltche
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
